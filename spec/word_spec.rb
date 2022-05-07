50f9526dbb4f2e6f30a7f39beb936188fff3cc23
Greg Khmara <gkhmara@gmail.com>
2022-05-07 13:30:46 -0700

    add second test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,9 +2,19 @@ require 'rspec'
 require 'word'
 
 describe '#Word' do
-  describe ('.all') do
+  describe('.all') do
     it("returns an empty array when there are no words") do
       expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("Hat", nil)
+      word.save()
+      word2 = Word.new("Cat", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
     end
   end
 

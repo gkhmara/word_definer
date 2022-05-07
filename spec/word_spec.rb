dbebd2eec400997f4fb48384dd661fbcad6e41b8
Greg Khmara <gkhmara@gmail.com>
2022-05-07 15:52:54 -0700

    add a before each block to spec file.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require 'word'
 
 describe '#Word' do
+
+  before(:each) do
+    Word.clear()
+  end
+
   describe('.all') do
     it("returns an empty array when there are no words") do
       expect(Word.all).to(eq([]))

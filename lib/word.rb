6a6df741bc5e54020f3c6fb04cac881a219df16a
Greg Khmara <gkhmara@gmail.com>
2022-05-07 15:44:49 -0700

    add save method, equality method, and id variable.

--- lib/word.rb
+++ lib/word.rb
@@ -1,12 +1,24 @@
 class Word
-  @@words = {}
+  attr_reader :id, :name
 
-  def initialize(name)
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1
   end
 
   def self.all
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
   end
 
 end

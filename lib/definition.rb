aad78654f86bcaec2eb8822b40a26c56b0305f86
Greg Khmara <gkhmara@gmail.com>
2022-05-07 17:57:27 -0700

    add a new method.

--- lib/definition.rb
+++ lib/definition.rb
@@ -11,6 +11,16 @@ class Definition
     @id = id || @@total_rows += 1
   end
 
+  def self.find_by_word(wrd_id)
+    definitions = []
+    @@definitions.values.each do |definition|
+      if definition.word_id == wrd_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
   def ==(definition_to_compare)
     (self.name() == definition_to_compare.name()) && (self.word_id() === definition_to_compare.word_id())
   end

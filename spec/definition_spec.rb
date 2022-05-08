10d66e0333562342b6f8ec3cb2922e47f8c6bb6c
Greg Khmara <gkhmara@gmail.com>
2022-05-07 17:04:30 -0700

    add clear to tests

---
+++ spec/definition_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'definition'
+require 'word'
+require 'pry'
+
+describe '#Definition' do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word = Word.new("Hat", nil)
+    @word.save()
+  end
+
+end

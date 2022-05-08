40509ec7170ac5095e9277e2c17d59cd79f59f64
Greg Khmara <gkhmara@gmail.com>
2022-05-07 18:37:24 -0700

    adding new routes

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -49,3 +50,12 @@ delete('/words/:id') do
   @words = Word.all
   erb(:words)
 end
+
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:song_name])

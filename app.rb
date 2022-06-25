5e0948806e3ccd7767d38d6a15dcdebd9913e869
Greg Khmara <gkhmara@gmail.com>
2022-06-25 13:39:20 -0700

    update app.rb

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ get('/words/new') do
 end
 
 get('/words/:id') do
-  @word = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   erb(:word)
 end
 
@@ -33,46 +33,46 @@ post('/words') do
 end
 
 get('/words/:id/edit') do
-  @word = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   erb(:edit_word)
 end
 
 patch('/words/:id') do
-  @word = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   @word.update(params[:name])
   @words = Word.all
-  erb[:words]
+  erb(:words)
 end
 
 delete('/words/:id') do
-  @word = Album.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   @word.delete()
   @words = Word.all
   erb(:words)
 end
 
 get('/words/:id/definitions/:definition_id') do
-  @definition = Definition.find(params[:definition_id].to_i())
+  @definition = Definition.find(params[:definition_id].to_i)
   erb(:definitions)
 end
 
 post('/words/:id/definitions') do
-  @word = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   definition = Definition.new(params[:definition_name], @word.id, nil)
   definition.save()
   erb(:word)
 end
 
 patch('/words/:id/definitions/:definition_id') do
-  @word = Word.find(params[:id].to_i())
-  definition = Definition.find(params[:definition_id].to_i())
+  @word = Word.find(params[:id].to_i)
+  definition = Definition.find(params[:definition_id].to_i)
   definition.update(params[:name], @word.id)
   erb(:word)
 end
 
 delete('/words/:id/definitions/:definition_id') do
-  definition = Definition.find(params[:definition_id].to_i())
+  definition = Definition.find(params[:definition_id].to_i)
   definition.delete
-  @word = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i)
   erb(:word)
 end
